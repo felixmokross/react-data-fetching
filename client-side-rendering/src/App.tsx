57b5ef8d1eb7dd48e4c637adcb4f18e58972cb01
Felix Mokross <felix.mokross@zuehlke.com>
2022-12-22 22:05:20 +0100

    Add react router and tanstack query

--- client-side-rendering/src/App.tsx
+++ client-side-rendering/src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/tanstack-query" element={<TanstackQuery />} />
+        <Route path="/react-router" element={<ReactRouter />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
+function Index() {
+  return (
+    <ul>
+      <li>
+        <Link to="tanstack-query">TanStack Query</Link>
+      </li>
+      <li>
+        <Link to="react-router">React Router</Link>
+      </li>
+    </ul>
+  );
+}
+
+function TanstackQuery() {
+  return <p>TanStack Query</p>;
+}
+
+function ReactRouter() {
+  return <p>React Router</p>;
+}
