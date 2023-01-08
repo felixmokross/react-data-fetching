4a3a217b33ac7e1057cfa04c4ae33092f7423548
Felix Mokross <felix.mokross@zuehlke.com>
2023-01-08 17:11:39 +0100

    Add TanStack Query and React Router data loading examples

--- client-side-rendering/src/App.tsx
+++ client-side-rendering/src/App.tsx
@@ -1,36 +1,113 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import {
+  createBrowserRouter,
+  Link,
+  Navigate,
+  Outlet,
+  RouterProvider,
+  useLoaderData,
+  useNavigation,
+} from "react-router-dom";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Navigate to="tanstack-query" replace={true} /> },
+      { path: "tanstack-query", element: <TanstackQuery /> },
+      {
+        path: "react-router",
+        loader: ({ request }) =>
+          fetch("https://date.nager.at/api/v3/nextpublicholidays/ch", {
+            signal: request.signal,
+          }),
+        element: <ReactRouter />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/tanstack-query" element={<TanstackQuery />} />
-        <Route path="/react-router" element={<ReactRouter />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
 
 export default App;
 
-function Index() {
+function Layout() {
+  const navigation = useNavigation();
+  return (
+    <>
+      <nav>
+        <Link to="tanstack-query">TanStack Query</Link> |{" "}
+        <Link to="react-router">React Router</Link>
+        {navigation.state === "loading" && " | Loading…"}
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+type PublicHolidayType =
+  | "Public"
+  | "Bank"
+  | "School"
+  | "Authorities"
+  | "Optional"
+  | "Observance";
+
+type PublicHoliday = {
+  date: string;
+  localName: string;
+  name: string;
+  countryCode: string;
+  fixed: boolean;
+  global: boolean;
+  counties: string[];
+  launchYear: number;
+  types: PublicHolidayType[];
+};
+
+function TanstackQuery() {
+  const { data, status } = useQuery({
+    queryKey: ["holidays", "ch"],
+    queryFn: async () =>
+      (await (
+        await fetch("https://date.nager.at/api/v3/nextpublicholidays/ch")
+      ).json()) as PublicHoliday[],
+  });
+
+  if (status === "loading") return <p>Loading…</p>;
+  if (status === "error") return <p>An error occurred!</p>;
+
   return (
     <ul>
-      <li>
-        <Link to="tanstack-query">TanStack Query</Link>
-      </li>
-      <li>
-        <Link to="react-router">React Router</Link>
-      </li>
+      {data.map((holiday) => (
+        <li key={holiday.date}>{holiday.name}</li>
+      ))}
     </ul>
   );
 }
 
-function TanstackQuery() {
-  return <p>TanStack Query</p>;
-}
-
 function ReactRouter() {
-  return <p>React Router</p>;
+  const data = useLoaderData() as PublicHoliday[];
+  return (
+    <ul>
+      {data.map((holiday) => (
+        <li key={holiday.date}>{holiday.name}</li>
+      ))}
+    </ul>
+  );
 }
