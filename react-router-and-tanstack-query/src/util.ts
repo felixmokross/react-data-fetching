1a3da7312759323e39e7c5e65ea790d5d6fb15ac
Felix Mokross <felix.mokross@zuehlke.com>
2023-05-12 10:07:46 +0200

    Remove React Router and TanStack Router combined example

--- react-router-and-tanstack-query/src/util.ts
+++
@@ -1,6 +0,0 @@
-export async function get(url: string) {
-  const res = await fetch(`https://date.nager.at/api/v3/${url}`);
-  if (!res.ok) throw new Error(`API returned ${res.status}`);
-
-  return await res.json();
-}
